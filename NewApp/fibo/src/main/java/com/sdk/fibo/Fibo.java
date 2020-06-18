656b601ffc7ea8cd6ff78efca9a293e5654bd842
Shubham Srivastava <smjackson94@gmail.com>
2020-06-18 10:05:11 +0530

    first

---
+++ NewApp/fibo/src/main/java/com/sdk/fibo/Fibo.java
@@ -0,0 +1,52 @@
+package com.sdk.fibo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+
+import org.json.JSONObject;
+
+public class Fibo extends AppCompatActivity {
+
+    private FiboAPI view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public Fibo(FiboAPI view) {
+        this.view = view;
+        view.addJavascriptInterface(this, "FibodroidAPI");
+    }
+
+    @JavascriptInterface
+    public void hide() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                view.hide();
+            }
+        });
+    }
+
+    public void setTrue() {
+        this.view.setTrue();
+    }
+
+    public void setEvent(String name, String val, String params) {
+        this.view.setEvent(name, val, params);
+    }
+
+    public void setEvent(String name, String val) {
+        this.view.setEvent(name, val, "");
+    }
+
+    public void setEvent(String name) {
+        this.view.setEvent(name, "", "");
+    }
+}
